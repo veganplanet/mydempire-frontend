b71066c6c8b78c3d561254c8af3f2c05d6b9145c
veganplanet <33604839+veganplanet@users.noreply.github.com>
2026-03-16 22:59:50 +0400

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,17 @@
+let lastScroll = 0;
 
+const navbar = document.querySelector(".appbar");
+
+window.addEventListener("scroll", () => {
+
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > lastScroll && currentScroll > 80) {
+    navbar.classList.add("appbar-hidden");
+  } else {
+    navbar.classList.remove("appbar-hidden");
+  }
+
+  lastScroll = currentScroll;
+
+});
